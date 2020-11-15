3b30ebdae89ac88c9e627e4366e199feb09fc6b8
Lwolwo <26643253+Lwolwo@users.noreply.github.com>
2020-11-15 21:16:16 +0800

    feat: finish test

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,20 @@
 import { register } from "../user";
+import { verifyPassword } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    await expect(register("username", "pwd")).resolves.toEqual({});
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyPassword.mockReturnValue(false);
+    await expect(register("username", "pwd")).rejects.toThrowError(
+      "wrong username or password"
+    );
   });
 });
